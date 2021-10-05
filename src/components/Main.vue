47e2a849650bcc1766b7d098da6c8458b20b291f
manfred-commits <manfredipiraino@gmail.com>
2021-10-05 17:17:41 +0200

    added watch hook, on userInput, for api call

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,12 +7,34 @@
 
 <script>
 import Film from './Film.vue';
+import axios from 'axios'
 
 export default {
     name:'Main',
     props:['headerInput'],
+    data(){
+      return{
+
+      }
+    },
     components:{
         Film,
+    },
+    methods:{
+   
+    },
+    watch:{
+      headerInput:function(){
+        axios
+        .get('/user',{
+          params: {
+          ID: 12345
+        }
+        })
+        .then(function (response) {
+        console.log(response);
+        })
+      },
     }
 }
 </script>
