bf4aa8dd60ffb6cbf29ddd20e835a31233259cdd
manfred-commits <manfredipiraino@gmail.com>
2021-10-06 15:15:25 +0200

    added basic header scss style & imports

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <header>
       <div class="col-left">
-          <img src="" alt="">
+          <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-1-1.png" alt="">
       </div>
       <div class="col-right">
-          <input type="text" v-model="userInput">
-          <button @click="$emit('input',userInput)">Invio</button>
+          <input type="text" v-model="userInput" @keyup.enter="$emit('input',userInput)">
       </div>
   </header>
 </template>
@@ -24,5 +23,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: .625rem 0;
+    .col-left{
+        margin-left: .625rem;
+        img{
+            object-fit: contain;
+            height: 100%;
+            width: 9.375rem;
+        }
+    }
+    .col-right{
 
+        input{
+            margin-right: .625rem;
+        }
+    }
+}
 </style>
