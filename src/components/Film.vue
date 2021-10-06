05b72e766a4a0c561adbb327c68a1dea53646c39
manfred-commits <manfredipiraino@gmail.com>
2021-10-06 13:43:04 +0200

    added poster images for Tv series and films

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="film-container" >
-    <!-- <img :src="'https://image.tmdb.org/t/p/w500'+film.backdrop_path" :alt="film.title"> -->
+    <img :src="'https://image.tmdb.org/t/p/w500'+film.backdrop_path" :alt="film.title?film.title:film.name" onerror="this.src != 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png'? this.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png' : '';">
     <h4 >Titolo:{{film.title?film.title:film.name}}</h4>
     <h4>Titolo Originale:{{film.original_title?film.original_title:film.original_name}}</h4>
     <div>Lingua Originale:
