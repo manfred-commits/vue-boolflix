7b4b282a35d25ad71371e7867de692b64fe36fa4
manfred-commits <manfredipiraino@gmail.com>
2021-10-06 13:37:54 +0200

    added condition series/movies "fine milestone 2"

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="film-container" >
     <!-- <img :src="'https://image.tmdb.org/t/p/w500'+film.backdrop_path" :alt="film.title"> -->
-    <h4>Titolo:{{film.title}}</h4>
-    <h4>Titolo Originale:{{film.original_title}}</h4>
+    <h4 >Titolo:{{film.title?film.title:film.name}}</h4>
+    <h4>Titolo Originale:{{film.original_title?film.original_title:film.original_name}}</h4>
     <div>Lingua Originale:
       <img onerror="this.src != 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png'? this.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png' : '';" :src="'https://www.unknown.nu/flags/images/'+film.original_language+'-100'" alt="">
     </div>
