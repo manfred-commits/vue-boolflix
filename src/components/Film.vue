8e1fecac22ef2cc42cada05c8395ef8fa5cf95b6
manfred-commits <manfredipiraino@gmail.com>
2021-10-06 10:05:48 +0200

    added language to flag icons

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -3,7 +3,9 @@
     <!-- <img :src="'https://image.tmdb.org/t/p/w500'+film.backdrop_path" :alt="film.title"> -->
     <h4>Titolo:{{film.title}}</h4>
     <h4>Titolo Originale:{{film.original_title}}</h4>
-    <div>Lingua Originale:{{film.original_language}}</div>
+    <div>Lingua Originale:
+      <img :src="'https://www.unknown.nu/flags/images/'+film.original_language+'-100'" alt="">
+    </div>
     <div class="score">Media Punteggio:{{film.vote_average}}</div>
   </section>
 </template>
