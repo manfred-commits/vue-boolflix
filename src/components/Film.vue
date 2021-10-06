c3cacacd303a8b9ba45467813d4ea7e7fb98a15e
manfred-commits <manfredipiraino@gmail.com>
2021-10-06 14:52:31 +0200

    added font awesome& score to star Fine milestone 3

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -6,25 +6,43 @@
     <div>Lingua Originale:
       <img onerror="this.src != 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png'? this.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag.svg/600px-Flag.svg.png' : '';" :src="'https://www.unknown.nu/flags/images/'+film.original_language+'-100'" alt="">
     </div>
-    <div class="score">Media Punteggio:{{averageToFive(film.vote_average)}}</div>
+    <div class="score">
+      Media Punteggio:{{averageToFive(film.vote_average)}}
+      <span>
+        <i class="fas fa-star" v-for="(star,index) in stars" :key="index+star" :class="index<averageToFive(film.vote_average)? 'yellow-star':'grey-star'"></i>
+        
+      </span>
+    </div>
   </section>
 </template>
 
 <script>
+import '@fortawesome/fontawesome-free/js/all.js';
 export default {
     name:'Film',
     props:['film'],
+    data(){
+      return{
+        stars:['','','','',''],
+      }
+    },
     methods:{
       averageToFive(averageScore){
         console.log(averageScore);
         console.log(Math.ceil(averageScore));
         return Math.ceil(averageScore/2);
-      }
+      },
+      
     }
 
 }
 </script>
 
 <style lang="scss" scoped>
-
+.yellow-star{
+  color: gold;
+}
+.grey-star{
+  color: grey;
+}
 </style>
